35270159333a9d7daae501210cdbe8f4bcb71c25
inyoungsJang <zxzxdlsdud@naver.com>
2019-12-06 17:26:34 +0900

    update listview data onItemClickListener 추가, 카운트 아이콘추가

--- app/src/main/java/com/example/mybooktag/DataInActivity.java
+++ app/src/main/java/com/example/mybooktag/DataInActivity.java
@@ -32,9 +32,11 @@ import java.util.ArrayList;
 
 public class DataInActivity extends AppCompatActivity {
     TextView tvShowData; //list클릭시 파일내용 보여줄 TextView
+    TextView tvFileNum;
     ListView listviewData;
+    int fileNum=0;
 
-    String sdcardBookTagPath = Environment.getExternalStorageDirectory().getPath() + "/BookTag/"; //저장경로
+    String sdcardBookTagPath = Environment.getExternalStorageDirectory().getPath() + "/BookTag/"; //sd카드의 파일 저장경로
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +44,7 @@ public class DataInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_data_in);
 
         tvShowData = (TextView) findViewById(R.id.tvShowData);
+        tvFileNum = (TextView) findViewById(R.id.tvFileNum);
         listviewData = (ListView) findViewById(R.id.listviewData);
 
         ActionBar actionBar = getSupportActionBar();
@@ -50,29 +53,14 @@ public class DataInActivity extends AppCompatActivity {
 
         SdcardBookList();
 
-        listviewData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String bookname;
-                String strText = (String) parent.getItemAtPosition(position) ; //리스트이름
-                try {
-                    FileInputStream fileInputStream = new FileInputStream(sdcardBookTagPath + strText); //
-                    byte[] txt = new byte[fileInputStream.available()];
-                    fileInputStream.read(txt);
-                    tvShowData.setText(new String(txt));
-                    fileInputStream.close();
 
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(), "오류", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
 
     } //onCreate END
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { //?
         super.onCreateOptionsMenu(menu);
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.datainmenu, menu);
@@ -82,12 +70,8 @@ public class DataInActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-/*        String sdcardPath = Environment.getRootDirectory().getAbsolutePath() + "/BookTag";
-        File[] files = (new File(sdcardPath).listFiles());
-        String strFileName;*/ //???????????????????????????????????????????????????????????????
-
         switch (item.getItemId()) {
-            case android.R.id.home:
+            case android.R.id.home: //안드로이드스튜디오에서 지원해주는 메서드를이용하여, 액션바 뒤로가기버튼 클릭시 홈으로
                 finish();
                 break;
             case R.id.item_Upgrade:
@@ -118,6 +102,7 @@ public class DataInActivity extends AppCompatActivity {
     }*/
 
     void SdcardBookList() {
+
         final ArrayList<String> bookName = new ArrayList<String>();// text파일 이름
         File[] listFile = new File(sdcardBookTagPath).listFiles(); //sdcardBookTagPath의 경로에있는 파일을 불러옴
         String fileName, extName; //
@@ -127,18 +112,30 @@ public class DataInActivity extends AppCompatActivity {
             extName = fileName.substring(fileName.length() - 3); //확장자만 걸러옴
             if (extName.equals("txt")) { //걸러온 확장자가 'txt' 단어이면 참
                 bookName.add(fileName);
+                fileNum++;
             }
         }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, bookName);
+
         listviewData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-// TODO: 2019-12-02 아이템 클릭시 클릭된 항목 배경색 변경
+                String strText = (String) parent.getItemAtPosition(position); //리스트이름
+                try {
+                    FileInputStream fileInputStream = new FileInputStream(sdcardBookTagPath + strText); //fileInputStream에 읽어올 파일 대입
+                    byte[] txt = new byte[fileInputStream.available()]; //해당파일의 내용 모두
+                    fileInputStream.read(txt);
+                    tvShowData.setText(new String(txt));
+                    fileInputStream.close();
 
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "오류!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+        tvFileNum.setText(fileNum); //????????????오류
+        adapter.notifyDataSetChanged();
         listviewData.setAdapter(adapter);
     }
-
-
 }
